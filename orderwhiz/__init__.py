0a0002d8328a601bbc2e1357e26750fe7022f591
plengxvi <114078929+plengxvi@users.noreply.github.com>
2023-07-11 18:13:13 -0400

    [AM-39]: Initial commit

---
+++ orderwhiz/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+import os,sys,requests, json
+from random import randint
+import speech
+
+app = Flask(__name__)
+
+@app.route('/')
+def print_greeting():
+    return render_template('index.html')
+
+@app.route('/parse', methods=['POST', 'GET'])
+def extract():
+      text=str(request.form.get('value1'))
+      payload = json.dumps({"sender": "Rasa","message": text})
+      headers = {'Content-type': 'application/json', 'Accept':     'text/plain'}
+      response = requests.request("POST",   url="http://localhost:5005/webhooks/rest/webhook", headers=headers, data=payload)
+      response=response.json()
+      resp=[]
+      for i in range(len(response)):
+        try:
+          resp.append(response[i]['text'])
+        except:
+          continue
+      result=resp
+      return render_template('index.html', result=result,text=text)
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
